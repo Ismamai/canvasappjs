e70b400d383927c3497ce1b82e9dd11e1f65a929
iblesa <iblesa@espatial.com>
2019-03-19 16:14:18 +0000

    Removing Sfdc.canvas method as it fails Added script on index.html to react on clicking on the paragraph to go to second page Added second page with paragraph and code to call history.back()

--- assets/app.js
+++ assets/app.js
@@ -6,9 +6,3 @@ document.getElementById('submitButton').addEventListener("click",function(e){
   console.log("Hello");
   e.preventDefault();
 });
-
-Sfdc.canvas(function() {
-    // Save the token
-    sfdc.canvas.oauth.token(window.signedRequestJson.oauthToken);
-    window.alert("hello, " + window.signedRequestJson.context.user.fullName);
-});
