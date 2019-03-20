f416019cc2122f6c77ee16c8b367c7b1aafa76ae
iblesa <iblesa@espatial.com>
2019-03-20 16:59:43 +0000

    Addring directorRouter

--- assets-dev/apps.js
+++ assets-dev/apps.js
@@ -2,4 +2,18 @@
 
 var MobileScreensComponent = sampleViews.FullScreens;
 
+var routes = {
+    '/Next/:value': function(value) {
+        console.log("Next screen." +value);
+    },
+    '' : function () {
+        console.log("initial screen");
+    }
+};
+
+sampleViews.appRouter = new DirectorRouter(routes);
+
+sampleViews.appRouter.init();
+
+
 ReactDOM.render(<MobileScreensComponent/>, document.getElementById("mobileScreen"));
--- assets/apps.js
+++ assets/apps.js
@@ -1,6 +1,19 @@
-"use strict";
+'use strict';
 
 var MobileScreensComponent = sampleViews.FullScreens;
+
+var routes = {
+    '/Next/:value': function NextValue(value) {
+        console.log("Next screen." + value);
+    },
+    '': function _() {
+        console.log("initial screen");
+    }
+};
+
+sampleViews.appRouter = new DirectorRouter(routes);
+
+sampleViews.appRouter.init();
 
 ReactDOM.render(React.createElement(MobileScreensComponent, null), document.getElementById("mobileScreen"));
 //# sourceMappingURL=apps.js.map
