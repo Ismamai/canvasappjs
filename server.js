db18cac67147a072ce4d407b54230151119c6b56
iblesa <iblesa@espatial.com>
2019-05-10 14:15:32 +0100

    Adding code to deal with signed request from nodejs app

--- server.js
+++ server.js
@@ -1,17 +1,35 @@
 const express = require('express');
-
+const bodyParser = require('body-parser'),
+    path = require('path'),
+    CryptoJS = require("crypto-js");
 var app = express();
+var consumerSecret = process.env.CANVAS_CONSUMER_SECRET;
+
 const port = process.env.PORT || 3000;
 // middleware
 // __dirname - stores path to directory
 app.use(express.static(__dirname + '/assets'));
 
-app.get('/',(req,res)=>{
-  res.send('hello Express');
-});
+//https://rajvakati.com/2017/10/19/salesforce-node-js-canvas-signed-request/
 
-app.post('/',(req,res)=>{
-  console.log("In post method");
-  res.send('hello Express');
+// app.use(express.static(path.join(__dirname, '/')));
+app.set('view engine', 'ejs');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ entended: true }));
+
+app.get('/', function (req, res) {
+  res.render('index');
 });
+app.post('/', function (req, res) {
+  var signed_req = req.body.signed_request;
+  var hashedContext = signed_req.split('.')[0];
+  var context = signed_req.split('.')[1];
+  var hash = CryptoJS.HmacSHA256(context, consumerSecret);
+  var b64Hash = CryptoJS.enc.Base64.stringify(hash);
+  if (hashedContext === b64Hash) {
+    res.render('index', { req: req.body, res: res.data });
+  } else {
+    res.send("authentication failed");
+  };
+})
 app.listen(port);
