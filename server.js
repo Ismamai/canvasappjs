dc2d05c356bd668cd7f8c6e60481b0286db536ee
iblesa <iblesa@espatial.com>
2019-05-10 11:52:43 +0100

    Checking if we receive the post method

--- server.js
+++ server.js
@@ -9,4 +9,9 @@ app.use(express.static(__dirname + '/assets'));
 app.get('/',(req,res)=>{
   res.send('hello Express');
 });
+
+app.post('/',(req,res)=>{
+  console.log("In post method");
+  res.send('hello Express');
+});
 app.listen(port);
