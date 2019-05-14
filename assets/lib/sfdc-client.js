cc020390c3046de328d61912f8aa72d2c5d4d4d0
iblesa <iblesa@espatial.com>
2019-05-14 15:01:00 +0100

    Removing alerts and

--- assets/lib/sfdc-client.js
+++ assets/lib/sfdc-client.js
@@ -1,6 +1,6 @@
 'use strict';
 var sfdcClient = function () {
-    console.log('Initializing sfdcClient');
+    console.log('Initializing sfdcClient...');
 
     initialize();
 
@@ -53,17 +53,6 @@ var sfdcClient = function () {
         publish(event.channel, event.payload);
     }
 
-    /**
-     * It receives an object with the following elements
-     * {
-     *      action: action from singleSFActions
-     *      object: object from sfObjects
-     *      params: all parameters that will be needed to perform the action on
-     * }
-     * and creates an event to communicate with remote visualforce page where the application
-     * is running. This will let us use sforce.one api or windows.location
-     * @param operation object that contains all information
-     */
     function openObject(objectId) {
         var operation = {
             object: 'Lead',
--- assets/sfdc.js
+++ assets/sfdc.js
@@ -2,7 +2,7 @@ var signedRequestJson = JSON.parse(Sfdc.canvas.decode(signedRequest, consumerSec
 Sfdc.canvas(function() {
     // Save the token
     Sfdc.canvas.oauth.token(window.signedRequestJson.client.oauthToken);
-    window.alert("hello, " + window.signedRequestJson.context.user.fullName);
+    // window.alert("hello, " + window.signedRequestJson.context.user.fullName);
     console.log("hello, " + window.signedRequestJson.context.user.fullName);
 });
 var es = {
